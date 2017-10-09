3fb10984b6c7d173c8dabd28b717bbc2b6e4e1f2
acollins41198 <acollins41198@gmail.com>
2017-10-09 13:26:28 -0400

    Add files via upload

--- guessGame/guessGame.js
+++ guessGame/guessGame.js
@@ -1,5 +1,6 @@
 var started = false;
 var num;
+var tries = 0;
 $("document").ready(function(){
 	$("#game").hide();
 });
@@ -7,37 +8,50 @@ $("#gen").click(function(){
 	num = Math.floor((Math.random()*100) + 1);
 	if (!started){
 		$("#game").show(100);
-		var started = true;
+		$("#guessArea").show(100);
+		started = true;
 	} else {
-		$("#status").empty();
+		tries = 0;
+		$(".result").hide();
+		$("#guessArea").show(100);
 	}
-	console.log(num);
+	$("#numGuesses").html(tries);
+	//console.log(num); //Left in for debugging purposes
 
 });
 
 $("#custom").click(function(){
-	var num = Number($("#cNum").val());
+	
+	num = Number($("#cNum").val());
 	if (!started){
 		$("#game").show(100);
-		var started = true;
+		started = true;
 	} else {
-		$("#status").empty();
+		tries = 0;
+		$("#guessArea").show(100);
+		$("#game").show(100)
+		$(".result").hide();
 	}
-	console.log(num);
+	//console.log(num);
+	$("#numGuesses").html(tries);
+	$("#cNum").val("")
 
 });
 $("#sub").click(function(){
 	var guess = Number($("#guess").val());
+	tries++;
+	$("#numGuesses").html(tries);
 	if (guess == "" || guess == null){
 		$("#status").prepend("<br><br><span>Please insert a guess</span>");
 	}
 	console.log(guess);
 	if (guess > num){
 		console.log("high");
-		$("#status").prepend("<br><br><span>" + guess + " is too high</span>");
+		$("#status").prepend('<br><br><span class="result">' + guess + ' is too high</span>');
 	} else if (guess < num){
-		$("#status").prepend("<br><br><span>" + guess + " is too low</span>");
+		$("#status").prepend('<br><br><span class="result">' + guess + ' is too low</span>');
 	} else {
-		$("#status").prepend("<br><br><span>" + guess + " is correct! You Win!</span>");
+		$("#status").prepend('<br><br><span class="result"><strong>' + guess + ' is correct! You Win!</strong></span>');
+		$("#guessArea").hide();
 	}
 });
